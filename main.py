9f757da9176ff3d3d3e4de0fd33a050c4f55814c
LuisLeon-Dev <lleon5073@gmail.com>
2024-04-05 15:25:45 -0600

    add: collision with the snake tail

--- main.py
+++ main.py
@@ -37,8 +37,16 @@ while game_is_on:
         score_board.increase_score()
 
     # Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         score_board.game_over()
+
+    # Detect collision with the tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            score_board.game_over()
 
 screen.exitonclick()
